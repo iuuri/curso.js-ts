e8e6ee0cbfcadd593cdc0b3cba2b891b30caea05
iuri <iuri.souzasantos@gmail.com>
2024-01-09 21:43:40 -0300

    Feito até a aula 7

---
+++ JS/aula6/index.js
@@ -0,0 +1,31 @@
+let nome = 'João';
+
+console.log(nome, 'naceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2015');
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
+
+/************************************************************/
+
+//iniciando variavem sem valor
+let nomeSemValor;
+nome = 'Qualquer valor';
+console.log(nome);
+
+/************************************************************/
+
+//trocando variavel já declarada 
+
+let declarandoNovaVariavel = 'Joao';
+declarandoNovaVariavel = 'Iuri';
+console.log(declarandoNovaVariavel)
+
+//não podemos criar variaveis com palavras reservadas
+//variaveis precisam ter nomes significativos
+//não pode começar o nome de uma variavel com um numero 
+//não podem conter espaços ou traços
+//utilizamos camelCase
+//a linguagem é Case-sensitive
+//Não podemos redeclarar variaveis com let 
+//não utilizamos mais VAR para declarar variaveis. 
---
+++ JS/aula7/index.js
@@ -0,0 +1,13 @@
+//não podemos criar constantes com palavras reservadas
+//constantes precisam ter nomes significativos
+//não pode começar o nome de uma constante com um numero 
+//não podem conter espaços ou traços
+//utilizamos camelCase
+//a linguagem é Case-sensitive
+//Não pode modificar o valor de uma constante
+//não utilizamos mais VAR, utilize CONST. 
+
+const nome = 'João';
+console.log(nome);
+
+console.log(typeof nome); // "typeof" mostrar o tipo de dado inserido na variavel ou constante.
